e4e50540dd2ea7e06bd698bdd983952cd654787c
fatihgenc <gnc.fath@gmail.com>
2022-09-24 22:12:29 +0300

    create docker compose file for both redis and node

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const VISITS = 'visits'
+const PORT = 8080
 
 const express = require('express')
 const redis = require('redis')
 
-
 const app = express()
-const client = redis.createClient()
+const client = redis.createClient({
+    host: 'redis-server',
+    port: 6379
+})
 client.set(VISITS, 0)
 
 app.get('/', (req, res) => {
     client.get(VISITS, (err, visits) => {
+        if(err) return res.send({
+            success: false,
+            error: JSON.stringify(err, null, 2)
+        })
         res.send('Number of visits is ' + visits)
-        client.set(VISITS, +(visits+1))
+        client.set(VISITS, parseInt(visits)+1)
     })
 })
 
-app.listen(8080, console.log('Listening 8080'))
+app.listen(PORT, console.log(`Listening on ${PORT}`))
